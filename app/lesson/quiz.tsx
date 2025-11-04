3da6dd0558941f068d6d50e8316684cc890683c9
Udaykiran <peddintiudaykiran176@gmail.com>
2025-11-04 17:19:08 +0530

    Update quiz.tsx

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -205,7 +205,7 @@ export const Quiz = ({
 
   const title =
     challenge.type === "ASSIST"
-      ? "Select the correct meaning"
+      ? "Select the correct answer"
       : challenge.question;
 
   return (
@@ -220,7 +220,7 @@ export const Quiz = ({
 
       <div className="flex-1">
         <div className="flex h-full items-center justify-center">
-          <div className="flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
+          <div className="flex w-full flex-col gap-y-2 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
             <h1 className="text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl">
               {title}
             </h1>
